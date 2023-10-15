0cc9c55f978d53b779dde7aa48a49e38a7afc15d
david <david.rodini@buonarroti.tn.it>
2023-10-15 17:39:42 +0200

    Costruttore Pesce

--- src/Pesce.java
+++ src/Pesce.java
@@ -1,2 +1,17 @@
 public class Pesce {
+
+    private String tipo;
+    private double peso;
+
+    public Pesce(String tipo, double peso) throws Exception{
+        this.tipo = tipo;
+
+        if(peso <= 0){
+            throw new Exception("Valore non valido");
+        }
+
+        this.peso = peso;
+    }
+
+
 }
