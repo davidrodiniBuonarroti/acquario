4d7b6045ac19470d7567df5b586b3cbd2b12b094
david <david.rodini@buonarroti.tn.it>
2023-10-15 17:24:55 +0200

    Partenza progetto

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
 public class Main {
-   /* Pesce pesce1 = new Pesce("Rosso", 3);
-    Pesce pesce2 = new Pesce("Spada", 10);
-    Pesce pesci[] = {pesce1, pesce2};
 
-    Acquario acquario = new Acquario(pesci, 20);
-
-    System.out.println(acquario.toString());*/
 }
