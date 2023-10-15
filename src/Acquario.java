a79b9edb5cc24896bf976a58fa0321936b334eac
david <david.rodini@buonarroti.tn.it>
2023-10-15 17:52:20 +0200

    Costruttore Acquario

--- src/Acquario.java
+++ src/Acquario.java
@@ -3,4 +3,19 @@ public class Acquario {
     private Pesce pesci[];
     private double volume;
 
+    public Acquario(int dim, double volume) throws Exception{
+        if(dim <= 0){
+            throw new Exception("Valore non valido");
+        }
+
+        this.pesci = new Pesce[dim];
+
+        if(volume <= 0){
+            this.volume = volume;
+        }
+
+        this.volume = volume;
+    }
+
+
 }
